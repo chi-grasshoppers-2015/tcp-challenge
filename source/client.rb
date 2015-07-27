e4aabf6c44c721922c50836a80465b3fa7625472
Josh Jones & Matt Bogen <pair+z3ntech.zebogen@devbootcamp.com>
2015-07-27 10:24:45 -0500

    Complete challenge

--- source/client.rb
+++ source/client.rb
@@ -5,23 +5,33 @@ require 'socket'
 # '127.0.0.1' is an address that _always_ points to your own machine,
 # which is good since that's where our server will be running too.
 
-server_connection = TCPSocket.new('127.0.0.1', 2000)
+server_connection = TCPSocket.new('10.1.10.147', 2000)
+
+
 
 # Read a line from the server
-line_1 = server_connection.gets
+# line_1 = server_connection.gets
 
-# Read a second line from the server
-line_2 = server_connection.gets
+# # Read a second line from the server
+# line_2 = server_connection.gets
 
-line_3 = server_connection.gets
+# line_3 = server_connection.gets
 
 #Log out the first line we received from the server
-puts "Line 1: #{line_1}"
+# puts "Line 1: #{line_1}"
 
-#Log out the second line we received from the server
-puts "Line 2: #{line_2}"
+# #Log out the second line we received from the server
+# puts "Line 2: #{line_2}"
 
-puts "Line 3: #{line_3}"
+# puts "Line 3: #{line_3}"
+
+# server_connection.puts ARGV[0]
+# line_4 = server_connection.gets
+# puts "Line 4: #{line_4}"
+
+server_connection.puts ARGV.join(" ")
+response = server_connection.gets.chomp
+puts "Response from server: \"#{response}\""
 
 #Close the connection if we're done
 server_connection.close
--- source/server.rb
+++ source/server.rb
@@ -19,15 +19,33 @@ loop do
   puts "A client has connected"
 
   # Send a string to the client that connected.
-  client.puts "Greetings from #{STUDENT_1} and #{STUDENT_2}'s TCP server"
-  client.puts "Time is #{Time.now}"
-  client.puts "We are legion."
+  # client.puts "Greetings from #{STUDENT_1} and #{STUDENT_2}'s TCP server"
+  # client.puts "Time is #{Time.now}"
+  # client.puts "We are legion."
+  # echo = client.gets
+  # client.puts "Echo: #{echo.upcase}"
+
+  message = client.gets.chomp
+  puts "Received \"#{message}\""
+
+  response =
+    case message
+    when "RED HOT" then "H-O-T!"
+    when "DO IT AGAIN" then "Go, Fight, Win"
+    when "2 BITS" then "Holler!"
+    when "STOMP YOUR FEET" then "STOMP!"
+    end
+
+  puts "Sending \"#{response}\" in response"
+  client.puts response
 
   # Send some logging information to the terminal again
-  puts "We sent a greeting and the time. Closing connection."
+  # puts "We sent a greeting and the time. Closing connection."
 
   # Close the client connection
   client.close
 
   # Start the loop over so we can go back to `accept` and wait for a new client
 end
+
+#10.1.10.147
